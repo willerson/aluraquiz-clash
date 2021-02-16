53c37c717422536721c155b6bee3bef900e92b01
willerson <willerson.miranda@gmail.com>
2021-02-16 01:43:55 -0300

    adicionado quiz e useEffet

--- pages/quiz.js
+++ pages/quiz.js
@@ -1,45 +1,124 @@
+/* eslint-disable react/prop-types */
 /* eslint-disable linebreak-style */
-import React from 'react';
-import db from '../db.json';
-import Widget from '../src/components/Widget';
-import QuizBackground from '../src/components/QuizBackground';
-import QuizContainer from '../src/components/QuizContainer';
-import Button from '../src/components/Button'
+import React from "react";
+import db from "../db.json";
+import Widget from "../src/components/Widget";
+import QuizBackground from "../src/components/QuizBackground";
+import QuizContainer from "../src/components/QuizContainer";
+import Button from "../src/components/Button";
+import { func } from "prop-types";
+
+function LoadingWidget() {
+  return (
+    <Widget>
+      <Widget.Header>Carregando...</Widget.Header>
+
+      <Widget.Content>[Desafio do Loading]</Widget.Content>
+    </Widget>
+  );
+}
+
+function QuestionWidget({ question, totalQuestions, questionIndex, onSubmit }) {
+  const questionId = `question__${questionIndex}`;
+  return (
+    <Widget>
+      <Widget.Header>
+        <h3>{`Pergunta ${questionIndex + 1} de ${totalQuestions}`}</h3>
+      </Widget.Header>
+
+      <img
+        alt="Descrição"
+        style={{
+          width: "100%",
+          height: "150px",
+          objectFit: "cover",
+        }}
+        src={question.image}
+      />
+
+      <Widget.Content>
+        <h2>{question.title}</h2>
+        <p>{question.description}</p>
+
+        <form onSubmit={(infosDoEvento) => {
+          infosDoEvento.preventDefault();
+          onSubmit();
+        }}>
+          {question.alternatives.map((alternative, alternativeIndex) => {
+            const alternativeId = `alternative__${alternativeIndex}`;
+            return (
+              <Widget.Topic
+                as="label"
+                htmlFor={alternativeId}
+              >
+                <input
+                  // style={{display: 'none'}}
+                  id={alternativeId}
+                  name={questionId}
+                  type="radio"
+                />
+                {alternative}
+              </Widget.Topic>
+            );
+          })}
+
+          {/* <pre>
+          {JSON.stringify(question,null, 4)}
+        </pre> */}
+
+          <Button type="submit">Confirmar</Button>
+        </form>
+      </Widget.Content>
+    </Widget>
+  );
+}
+const screenStates = {
+  QUIZ: 'QUIZ',
+  LOADING: 'LOADING',
+  RESULT: 'RESULT',
+};
 
 export default function QuizPage() {
+  const [screenState, setScreenState] = React.useState(screenStates.LOADING);
+  const totalQuestions = db.questions.length;
+  const [currentQuestion, setCurrentQuestion] = React.useState(0);
+  const questionIndex = currentQuestion;
+  const question = db.questions[questionIndex];
+
+  React.useEffect(() => {
+    setTimeout(() => {
+      setScreenState(screenStates.QUIZ);
+    }, 1 * 1000);
+  }, []);
+
+  function handleSubmitQuiz() {
+    const nextQuestion = questionIndex + 1;
+    if (nextQuestion < totalQuestions) {
+      setCurrentQuestion(nextQuestion);
+    } else {
+      setScreenState(screenStates.RESULT);
+    }
+  }
+
   return (
     <QuizBackground backgroundImage={db.bg}>
       <QuizContainer>
-        <Widget>
-          <Widget.Header>
-            <h3>
-              Pergunta 1 de {`${db.questions.length}`}
-            </h3>
-          </Widget.Header>
 
-          <img 
-            alt="Descrição"
-            style={{
-              width: '100%',
-              height: '150px',
-              objectFit: 'cover',
-            }}
-            src="https://placehold.it/400x400"
+        {screenState === screenStates.QUIZ && (
+          <QuestionWidget
+            question={question}
+            totalQuestions={totalQuestions}
+            questionIndex={questionIndex}
+            onSubmit={handleSubmitQuiz}
           />
+        )}
 
-          <Widget.Content>
-            <h2>
-              Título
-            </h2>
-            <p>
-              Descrição
-            </p>
-            <Button>
-              Confirmar
-            </Button>
-          </Widget.Content>
-        </Widget>
+        {screenState === screenStates.LOADING && <LoadingWidget />}
+
+        {screenState === screenStates.RESULT && <div>Você acertou X questões, parabéns!</div>}
+
+        {/* <LoadingWidget /> */}
       </QuizContainer>
     </QuizBackground>
-  )
+  );
 }
